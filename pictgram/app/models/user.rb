cb14940b73271b49d87671f6d04a655733d8a379
名前 <>
2020-03-28 02:11:59 +0000

    rails07　質問

--- pictgram/app/models/user.rb
+++ pictgram/app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 15 }
+  validates :email, presence: true
+  validates :email, format: { with: /[a-zA-Z0-9._+]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*/ }
+  PW_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
+  validates :password, presence: true, format: { with: PW_REGEX }
+  
+  has_secure_password
+  
+  has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
 end
